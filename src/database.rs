589e2b45c1ca5bd0c6a48d4cfb11b64532deee1c
Norinoyaki <norinoyaki@proton.me>
2025-03-05 23:26:46 +0800

    Update github workflows method (2)

--- src/database.rs
+++ src/database.rs
@@ -1,11 +1,11 @@
-use std::path::Path;
+// use std::path::Path;
 
-use redb::{Database, TableDefinition};
+// use redb::{Database, TableDefinition};
 
 // const PLAYERS: TableDefinition<&str, &str> = TableDefinition::new("players");
 
-pub fn init_database(path: String) {
-    let working_path = format!("{}/data.db", path);
-    let db_path = Path::new(&working_path);
+pub fn init_database(_path: String) {
+    // let working_path = format!("{}/data.db", path);
+    // let db_path = Path::new(&working_path);
     // let db = Database::create(db_path).expect("Failed to initiate database.");
 }
